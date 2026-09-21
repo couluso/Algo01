bdef7b2d881e87cfd42a4b9931309e223c1b2bdf
Fénart Théo <t.fenart@rubika-edu.re>
2026-09-21 14:55:24 +0400

    Ajout vie, nom & position (TEST)

---
+++ Assets/Scripts/exercice1.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+using NUnit.Framework;
+using UnityEngine;
+
+public class exercice1 : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        string nomJoueur;
+        int vieJoueur = 100;
+
+        Vector3 positionJoueur;
+        positionJoueur = Vector3.zero;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
